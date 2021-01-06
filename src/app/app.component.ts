52a45e2bbad51f76b89aa50357b689a0836e6672
hind fathi <hind10bella.Fathi@gmail.com>
2021-01-06 14:37:09 +0100

    modify app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { ContactService } from './services/contact.service';
+import { Contact } from './config/config';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  listeContact: Array<{nom: string, prenom: string, numero: string, fav: boolean}> =
-    [{nom: 'PAULAIS', prenom: 'Jordan', numero: '0123456789', fav: false}];
+export class AppComponent implements OnInit{
+  listeContact: Contact[];
+
+  constructor(private service: ContactService){
+    ngOnInit(){
+      this.listeContact = this.service.getListContact();
+    }
+  }
 }
